7fc78df6adfbdea4eef9b38d314977e820725eb2
Minamototennsai <Minamototennsai@gmail.com>
2025-04-17 12:16:55 +0800

    fixed: slow query of week schedule, session list duplication.

--- src/main/java/g06/ecnu/heartbridge/service/NewManagerService.java
+++ src/main/java/g06/ecnu/heartbridge/service/NewManagerService.java
@@ -4,17 +4,17 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import g06.ecnu.heartbridge.DTO.ScheduleDTO;
 import g06.ecnu.heartbridge.entity.ConsultantDetail;
 import g06.ecnu.heartbridge.entity.Schedule;
 import g06.ecnu.heartbridge.mapper.ConsultantDetailMapper;
+import g06.ecnu.heartbridge.mapper.GetSevenDaysScheduleMapper;
 import g06.ecnu.heartbridge.mapper.ScheduleMapper;
 import g06.ecnu.heartbridge.mapper.UsersMapper;
 import jakarta.annotation.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +39,9 @@ public class NewManagerService {
 
     @Resource
     private ScheduleMapper scheduleMapper;
+
+    @Resource
+    private GetSevenDaysScheduleMapper getSevenDaysScheduleMapper;
 
     public ResponseEntity<Object> getDaily() {
         return ResponseEntity.ok("{\"num\": " + chatService.getDaily() + "}");
@@ -100,22 +103,16 @@ public class NewManagerService {
     }
 
     public ResponseEntity<Object> getScheduleWeek(){
-        LocalDate today = LocalDate.now();
-        LocalDate sevenDaysLater = today.plusDays(7);
-        QueryWrapper<Schedule> scheduleQueryWrapper = new QueryWrapper<>();
-        scheduleQueryWrapper.eq("agree", "accepted")
-                .ge("date", today)
-                .lt("date", sevenDaysLater);
-        List<Schedule> schedules = scheduleMapper.selectList(scheduleQueryWrapper);
         ObjectNode response = new ObjectMapper().createObjectNode();
+        List<ScheduleDTO> schedules = getSevenDaysScheduleMapper.getSevenDaysSchedule();
         ArrayNode schedulesArray = new ObjectMapper().createArrayNode();
-        for (Schedule schedule : schedules) {
+        for (ScheduleDTO scheduleDTO : schedules) {
             ObjectNode scheduleNode = new ObjectMapper().createObjectNode();
-            scheduleNode.put("fromid", schedule.getClientId());
-            scheduleNode.put("fromname", usersMapper.selectById(schedule.getClientId()).getUsername());
-            scheduleNode.put("toid", schedule.getClientId());
-            scheduleNode.put("toname", usersMapper.selectById(schedule.getConsultantId()).getUsername());
-            scheduleNode.put("time", ChronoUnit.DAYS.between(today, schedule.getDate()));
+            scheduleNode.put("fromid", scheduleDTO.getFromid());
+            scheduleNode.put("fromname", scheduleDTO.getFromname());
+            scheduleNode.put("toid", scheduleDTO.getToid());
+            scheduleNode.put("toname", scheduleDTO.getToname());
+            scheduleNode.put("date", scheduleDTO.getDate());
             schedulesArray.add(scheduleNode);
         }
         response.set("schedules", schedulesArray);
